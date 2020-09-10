e596352e72d101487509eee619d1a055f53205d9
Joseph Johny Emmatty <josinho.seven@gmail.com>
2020-09-10 06:18:47 +0530

    sample for string and types in python

---
+++ basic/string-functions.py
@@ -0,0 +1,7 @@
+myString = "heLLo String"
+
+print(myString)
+print(myString.upper())
+print(myString.lower())
+print(myString.title())
+print(myString.split(" "))
---
+++ basic/types.py
@@ -0,0 +1,9 @@
+a = 10
+b = "Hello"
+c = 1.5
+d = True
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
